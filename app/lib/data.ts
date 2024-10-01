d94be9db01d38f064dbec1730a7c7a342f45e1bc
Guido Pérez Moro <guidoperezmoro@gmail.com>
2024-10-01 17:23:59 -0300

    Chapter 11: Adding Search and Pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,7 +13,6 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
     console.log("Fetching revenue data...");
     await new Promise((resolve) => setTimeout(resolve, 1500));
 
@@ -30,7 +29,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
